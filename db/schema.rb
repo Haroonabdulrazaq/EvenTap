131870b8709ba915f129e0f4f9a4dc1368eddc13
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-11 00:48:36 +0100

    Add User Signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_232747) do
+ActiveRecord::Schema.define(version: 2021_07_10_233227) do
 
   create_table "events", force: :cascade do |t|
     t.text "name"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2021_07_10_232747) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "name"
+    t.text "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
