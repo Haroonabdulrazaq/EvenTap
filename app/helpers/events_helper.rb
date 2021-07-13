cf2d66e66ae40806e79735de8133247c3004018f
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2021-07-13 13:06:30 +0100

    Fix Description bug and style issue

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,14 @@
 module EventsHelper
+  def event_des(description)
+    desArr = description.split(" ")
+    if desArr.length > 20
+      desArr = desArr[0..20].join(" ")
+      continue ="..."
+      desArr = continue.prepend(desArr)
+      desArr
+    else
+      desArr = desArr.join(" ")
+      desArr
+    end  
+  end
 end
